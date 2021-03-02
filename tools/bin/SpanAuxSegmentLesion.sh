b97448f593d60f1ec28c73ae8201fe8ad7166f93
Ryan Cabeen <cabeen@gmail.com>
2021-03-02 08:28:33 -0800

    updates

--- tools/bin/SpanAuxSegmentLesion.sh
+++ tools/bin/SpanAuxSegmentLesion.sh
@@ -151,12 +151,12 @@ runit qit --verbose VolumeThreshold \
 	--input ${tmp}/lesion.medprob.nii.gz \
 	--mask ${tmp}/lesion.dil.mask.nii.gz \
 	--threshold ${sigmoidLowThreshLesion} \
-	--output ${tmp}/lesion.low.mask.nii.gz
-
-runit qit --verbose MaskDilate \
-	--input ${tmp}/lesion.low.mask.nii.gz \
-	--num ${numDilateLesion} \
 	--output ${tmp}/lesion.mask.nii.gz
+
+# runit qit --verbose MaskDilate \
+# 	--input ${tmp}/lesion.low.mask.nii.gz \
+# 	--num ${numDilateLesion} \
+# 	--output ${tmp}/lesion.mask.nii.gz
 
 echo "segmenting csf"
 
