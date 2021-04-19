3d122d4ce0c1d88da35641853940dcb006aac755
cabeen <cabeen@gmail.com>
2021-04-19 15:24:41 -0700

    updates

--- tools/bin/SpanMainRunAll.sh
+++ tools/bin/SpanMainRunAll.sh
@@ -11,14 +11,17 @@
 
 mybin=$(cd $(dirname ${0}); pwd -P)
 
-
 for s in cases/source/*/*; do 
+  subd=$(echo ${s} | sed 's/source/process/g')
   logd=$(echo ${s} | sed 's/source/log/g') 
-  mkdir -p ${logd}
-  qsubcmd --qlog ${logd} bash ${mybin}/SpanMainRun.sh \
-    --source  ${s} \
-    --correct $(echo ${s} | sed 's/source/correct/g') \
-    --subject $(echo ${s} | sed 's/source/process/g')
+
+  if [ ! -e ${subd}/standard.vis ]; then
+    mkdir -p ${logd}
+    qsubcmd --qlog ${logd} bash ${mybin}/SpanMainRun.sh \
+      --source  ${s} \
+      --correct $(echo ${s} | sed 's/source/correct/g') \
+      --subject $(echo ${s} | sed 's/source/process/g')
+  fi
 done
 
 ################################################################################
