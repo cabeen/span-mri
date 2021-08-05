eefbfc3308b4c4e514dfb9cdb5d0522edcc441d1
Ryan Cabeen <rcabeen@RyBookPro.local>
2021-08-04 23:26:49 -0700

    updates

--- docs/rcvd/2020-09-17-UT/rename.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cd /ifs/loni/postdocs/rcabeen/studies/current/span/pilot
-
-for s in $(cat lookup.txt); do mm=$(echo ${s} | cut -f 1 -d ,); ear=$(echo ${s} | cut -f 3 -d ,); for d in level1/*/${mm}; do mv $d $(dirname ${d})/${ear}; done done
--- docs/sent/2021-04-21/data/run.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-
-cd $(dirname $0)
-
-cp table.wide.csv table.clean.csv; for bad in $(for f in fail/*; do echo $(basename ${f%_t2*} | sed); done | sed 's/_/,/g' | awk 'BEGIN{FS=","; OFS=","}{print $2,$1,$3}' | sed 's/ //g'); do cat table.clean.csv | grep -v ${bad} > tmp; mv tmp table.clean.csv; done
