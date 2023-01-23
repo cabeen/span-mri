f0cbb596e1813d9ea796009271ef632dd165f9f2
Ryan Cabeen <cabeen@gmail.com>
2023-01-22 17:43:44 -0800

    adapted to local environment

--- bin/SpanAuxSegmentBrainLearn.sh
+++ bin/SpanAuxSegmentBrainLearn.sh
@@ -33,14 +33,14 @@ fi
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/usr/local/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('/Users/rcabeen/opt/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/usr/local/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/usr/local/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "/Users/rcabeen/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/Users/rcabeen/opt/anaconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/usr/local/anaconda3/bin:$PATH"
+        export PATH="/Users/rcabeen/opt/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
